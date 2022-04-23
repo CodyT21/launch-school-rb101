ae633a55fa95d65e791fe6301aec8c358a057f4c
Cody Thomas <cody.t.thomas@gmail.com>
2022-04-23 16:56:19 -0600

    Cleanup up result calculation code block, added main loop to allow for multiple operations without needing to reload program

--- lesson2/calculator.rb
+++ lesson2/calculator.rb
@@ -21,60 +21,73 @@ def valid_math_operation?(operation)
 end
 
 prompt('Welcome to the calculator app!')
-prompt('Functionality is limited to addition, subtraction, multiplication, and division of 2 numbers (integer or float).')
-num1 = nil
-num2 = nil
-# get the first number from the user
+prompt('Functionality is limited to addition, subtraction, multiplication, and
+division of 2 numbers (integer or float).')
+
+# main loop
 loop do
-  prompt('Enter the first number: ')
-  num1_str = gets.chomp
-  if valid_int?(num1_str)
-    num1 = num1_str.to_i
-    break
-  elsif valid_float?(num1_str)
-    num1 = num1_str.to_f
-    break
-  else
-    prompt('Invalid input. Please input either an integer or a float.')
+  num1 = nil
+  num2 = nil
+  # get the first number from the user
+  loop do
+    prompt('Enter the first number: ')
+    num1_str = gets.chomp
+    if valid_int?(num1_str)
+      num1 = num1_str.to_i
+      break
+    elsif valid_float?(num1_str)
+      num1 = num1_str.to_f
+      break
+    else
+      prompt('Invalid input. Please input either an integer or a float.')
+    end
   end
-end
 
-# get the second number from the user
-loop do
-  prompt('Enter the second number: ')
-  num2_str = gets.chomp
-  if valid_int?(num2_str)
-    num2 = num2_str.to_i
-    break
-  elsif valid_float?(num2_str)
-    num2 = num2_str.to_f
-    break
-  else
-    prompt('Invalid input. Please input either an integer or a float.')
+  # get the second number from the user
+  loop do
+    prompt('Enter the second number: ')
+    num2_str = gets.chomp
+    if valid_int?(num2_str)
+      num2 = num2_str.to_i
+      break
+    elsif valid_float?(num2_str)
+      num2 = num2_str.to_f
+      break
+    else
+      prompt('Invalid input. Please input either an integer or a float.')
+    end
   end
+
+  # get mathematical operation to perform
+  math_op = ''
+  loop do
+    prompt(<<-MSG
+      Which mathematical operation would you like to perform?
+        1 Add
+        2 Subtract
+        3 Multiply
+        4 Divide
+      MSG
+          )
+    math_op = gets.chomp
+    break if valid_math_operation?(math_op)
+
+    prompt('Invalid input. Please enter 1, 2, 3, or 4.')
+  end
+
+  # calculate and return result
+  result =
+    case math_op
+    when '1' then num1 + num2
+    when '2' then num1 - num2
+    when '3' then num1 * num2
+    when '4' then num1 / num2.to_f # ensure division is more accurate
+    end
+
+  prompt(result)
+
+  prompt('Would you like to perform another calucation? (Enter y to continue)')
+  break if !gets.chomp.downcase.start_with?('y')
 end
 
-# get mathematical operation to perform
-math_op = ''
-loop do
-  prompt('Which mathematical operation would you like to perform?
-          1 Add
-          2 Subtract
-          3 Multiply
-          4 Divide')
-  math_op = gets.chomp
-  break if valid_math_operation?(math_op)
-
-  prompt('Invalid input. Please enter 1, 2, 3, or 4.')
-end
-
-# calculate and return result
-result = nil
-case math_op
-when '1' then result = num1 + num2
-when '2' then result = num1 - num2
-when '3' then result = num1 * num2
-when '4' then result = num1 / num2.to_f # convert denominator to float to ensure division is not rounded
-end
-
-puts result
+prompt('Thank you for using the calculator app!')
