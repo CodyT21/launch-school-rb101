d32cecdd6ae36fb24b2a1ae3d776613a353e0b14
Cody Thomas <cody.t.thomas@gmail.com>
2022-04-24 17:40:22 -0600

    Moved prompt messages to new configuration file

--- lesson2/loan_calculator.rb
+++ lesson2/loan_calculator.rb
@@ -1,5 +1,12 @@
-# inputs: loan amount (float), annual percentage rate (percentage, not decimal), loan duration (years)
-# outputs: monthly payment
+=begin
+Loan Monthly Payment Calculator App
+
+Takes in user input for the loan amount, apr, and loan duration and
+outputs a string with the monthly payment
+=end
+
+require 'yaml'
+MESSAGES = YAML.load_file('loan_calculator_messages.yml')
 
 def prompt(message)
   puts "=> #{message}"
@@ -10,44 +17,46 @@ def valid_number?(str_num)
   str_num.match(num_pattern)
 end
 
+prompt(MESSAGES['welcome'])
+
 loan_amount = nil
 apr = nil
 loan_duration = nil
 
 # get loan amount
 loop do
-  prompt('Enter your loan amount: ')
+  prompt(MESSAGES['loan_amount_input'])
   str_loan_amount = gets.chomp
   if valid_number?(str_loan_amount)
     loan_amount = str_loan_amount.to_f
     break
   end
-  prompt('Invalid input. Enter only digits and do not include commas.')
+  prompt(MESSAGES['invalid_loan_amount'])
 end
 
 # get apr
 loop do
-  prompt('Enter your APR as a percent: ')
+  prompt(MESSAGES['apr_input'])
   str_apr = gets.chomp
   if valid_number?(str_apr)
     apr = str_apr.to_f / 100
     break
   end
-  prompt('Invalid input. Enter your percentage without the percent sign, rounded to 2 decimals.')
+  prompt(MESSAGES['invalid_apr'])
 end
 monthly_apr = apr / 12
 
 # get loan duration
 loop do
-  prompt('Enter your loan duration in years: ')
+  prompt(MESSAGES['loan_duration_input'])
   str_loan_duration = gets.chomp
   if valid_number?(str_loan_duration)
     loan_duration = str_loan_duration.to_f * 12
     break
   end
-  prompt('Invalid input. That does not look like a number of years.')
+  prompt(MESSAGES['invalid_duration'])
 end
 
 # calculate monthly payment
 monthly_payment = loan_amount * (monthly_apr / (1 - (1 + monthly_apr)**(-loan_duration)))
-prompt("Your monthly payment is : $#{monthly_payment.round(2)}")
+prompt(MESSAGES['monthly_payment'] + "$#{monthly_payment.round(2)}")
