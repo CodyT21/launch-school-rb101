a068070b45cb983dcdfbd38a07cd89d55e6ad6d0
Cody Thomas <cody.t.thomas@gmail.com>
2022-05-11 17:57:43 -0600

    Added logic for making a user controlled move

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -24,24 +24,60 @@ def display_board(board)
   end
 
   # output board as string over 5 lines
-  converted_board.each do |row|
+  converted_board.each_with_index do |row, index|
     p row.join(' | ')
-    p '- + - + -' unless converted_board.index(row) == board.length - 1
+    p '- + - + -' unless index == board.length - 1
   end
 end
 
 def valid_input?(move)
-  move.match(/^[012]$/)
+  move.match(/^[123]$/)
 end
 
-# takes in a validated user string index passed in as a 2 element array,
-# and current board layout
+# takes in a validated user index passed in as a 2 element array of
+# integers, and current board layout
 # outputs whether move interferes with any previous moves
 def valid_move?(index, board)
-  row = index[0].to_i - 1
-  col = index[1].to_i - 1
+  row = index[0]
+  col = index[1]
   board[row][col] == '0'
 end
 
-board = [['0', '0', '0'], ['1', '2', '0'], ['1', '2', '0']]
+prompt('Welcome to the Tic Tac Toe game!')
+prompt('You will take turns playing with a computer player.')
 
+board = [['0', '0', '0'], ['0', '0', '0'], ['0', '0' ,'0']]
+loop do # main loop
+  display_board(board)
+
+  # player move
+  row_str = ''
+  col_str = ''
+  row = nil
+  col = nil
+
+  loop do
+    # row of player move
+    prompt('Enter the row number for your next move (1, 2, or 3): ')
+    row_str = gets.chomp
+    if !valid_input?(row_str)
+      prompt("Invalid input. Enter only '1', '2', or '3'.")
+      next
+    end
+    
+    # col of player move
+    loop do
+      prompt('Enter the column number for your next move (1, 2, or 3): ')
+      col_str = gets.chomp
+      break if valid_input?(col_str)
+      prompt("Invalid input. Enter only '1', '2', or '3'.")
+    end
+
+    row = row_str.to_i - 1
+    col = col_str.to_i - 1
+    break if valid_move?([row, col], board)
+    prompt('Invalid move. That space has already been played. Try again.')
+  end
+  
+  board[row][col] = '1' # add player move to board
+end
