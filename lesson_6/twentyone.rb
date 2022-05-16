b52428bca86d7b5ff8a6cc6629cd6b7fcd2cc506
Cody Thomas <cody.t.thomas@gmail.com>
2022-05-15 19:21:15 -0600

    Added hit! method, refactored deal_hands! to pull out card dealing code into new method deal_cardfixed bug in display_hands when displaying dealer hand

--- lesson_6/twentyone.rb
+++ lesson_6/twentyone.rb
@@ -19,23 +19,36 @@ def initialize_deck
   }
 end
 
-# deal cards return a nested array of 2 sets of cards
-# method will alter the deck hash that is passed in
+# deal_hands! returns a nested array of 2 sets of hands
+# method will alter the deck hash that is passed in (through deal_card!)
 def deal_hands!(deck)
   hands = []
   2.times do # loop for dealer and player
     hand = []
     2.times do # loop for dealing 2 cards
-      suit = [:hearts, :diamonds, :clubs, :spades].sample
-      card = deck[suit].sample
-      card_index = deck[suit].index(card)
-      # binding.pry
-      deck[suit].delete_at(card_index)
-      hand << "#{card} of #{suit.to_s.capitalize}"
+      card = deal_card!(deck)
+      hand << card
     end
     hands << hand
   end
   hands # index 0 is the dealer hand, index 1 is the player hand
+end
+
+# deal_card will return a single card from the deck hash as a string
+# method will modify hash that is passed in
+def deal_card!(deck)
+  # check if cards for specific suit are still available
+  suit = nil
+  until suit
+    suit = deck.keys.sample
+    if deck[suit].empty?
+      suit = nil
+    end
+  end
+  card = deck[suit].sample
+  card_index = deck[suit].index(card)
+  deck[suit].delete_at(card_index)
+  "#{card} of #{suit.to_s.capitalize}"
 end
 
 # helper method to improve appearance of printing each element from
@@ -61,13 +74,30 @@ end
 def display_hands(hands)
   hands.each do |hand|
     if hands.index(hand) == 0 # dealer hand - display random card
-      prompt("Dealer has: #{hand.sample} and unknown card")
+      hand_length = hand.length
+      prompt("Dealer has: #{joinand(hand[1, hand_length])} and unknown card")
     else
       prompt("Player has: #{joinand(hand)}")
     end
   end
 end
 
+# hit! will mutate the hands array to add a card for the designated player/dealer
+# deck will also be mutated
+def hit!(hands, deck, player=true)
+  if player
+    hands[1] << deal_card!(deck)
+  else
+    hands[0] << deal_card!(deck)
+  end
+end
+
 # deck = initialize_deck
 # hands = deal_hands!(deck)
 # display_hands(hands)
+# hit!(hands, deck)
+# hit!(hands, deck)
+# hit!(hands, deck, false)
+# hit!(hands, deck, false)
+# display_hands(hands)
+# puts deck
