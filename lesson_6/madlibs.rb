f99d5ef67798844931e11022912f09cb7634bf77
Cody Thomas <cody.t.thomas@gmail.com>
2022-06-20 13:55:30 -0600

    Added logic for reading file and replacing text with random words

--- lesson_6/madlibs.rb
+++ lesson_6/madlibs.rb
@@ -15,5 +15,20 @@ will not be changed when this program is ran.
 
 NOUNS = %w(fox dog head leg)
 VERBS = %w(jumps lifts bits licks)
-ADJECTIVEs = %w(quick lazy sleepy ugly)
+ADJECTIVES = %w(quick lazy sleepy ugly)
 ADVERBS = %w(easily lazily noisily excitedly)
+
+file = File.open('madlibs_text.txt')
+file_text = file.readlines.map(&:chomp)
+madlibs_text = file_text.map do |line|
+  line.split.map do |word|
+    case
+    when word.include?('%{noun}') then word.gsub('%{noun}', NOUNS.sample)
+    when word.include?('%{verb}') then word.gsub('%{verb}', VERBS.sample)
+    when word.include?('%{adjective}') then word.gsub('%{adjective}', ADJECTIVES.sample)
+    when word.include?('%{adverb}') then word.gsub('%{adverb}', ADVERBS.sample)
+    else word
+    end
+  end.join(' ')
+end
+puts madlibs_text.join("\n")
